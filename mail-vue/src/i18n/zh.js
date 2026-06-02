9acdcf3b7204e69581ad1697807a49841d485496
hzj495 <hzj694444990@gmail.com>
2026-06-02 13:09:14 +0800

    Update zh.js

--- mail-vue/src/i18n/zh.js
+++ mail-vue/src/i18n/zh.js
@@ -299,7 +299,7 @@ const zh = {
     reqFailErrorMsg: '请求失败，请稍后再试',
     message: '邮件详情',
     language: '系统语言',
-    totalUserAccount: '{msg} 个',
+    totalUserAccount: '{used}/{total} 个',
     sendBanned: '已禁用',
     sendInternal: '仅站内',
     wrote: '来信',
